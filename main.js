c787d5f14ffefeae708e3f2b9f42044e7e3d95a6
saraalshref <saraelshref12@gmail.com>
2023-12-22 22:44:09 +0200

    s

--- main.js
+++ main.js
@@ -1,15 +1,36 @@
+function toggleDivs() {
+    var plusDiv = document.getElementById("plus");
+    var minaceDiv = document.getElementById("minace");
 
-function toggleDiv(){
-    var plusDiv = document.querySelector(".divtoggle");
-    var minaceDiv = document.querySelector(".minace");
-
-    if (plusDiv.style.display === "block" ) {
-        plusDiv.style.display = "none";
-        document.getElementById("minaceDiv").innerHTML("<h3>Pre Book your Baggage Donnating a small -</h3><p>Lorem ipsum dolor sit amet, consectetur adiping elitIn imperdiet interdum imperdiet ipsum generators on the Internet tend to repeat predefined chunks as necessary</p>")
-    } else {
+    if (plusDiv.style.display === "none" || plusDiv.style.display === "") {
         plusDiv.style.display = "block";
         minaceDiv.style.display = "none";
+    } else {
+        plusDiv.style.display = "none";
+        minaceDiv.style.display = "block";
     }
 }
+function toggleDivs2() {
+    var plusDiv2 = document.getElementById("plus2");
+    var minaceDiv2 = document.getElementById("minace2");
 
+    if (plusDiv2.style.display === "none" || plusDiv2.style.display === "") {
+        plusDiv2.style.display = "block";
+        minaceDiv2.style.display = "none";
+    } else {
+        plusDiv2.style.display = "none";
+        minaceDiv2.style.display = "block";
+    }
+}
+function toggleDivs3() {
+    var plusDiv3 = document.getElementById("plus3");
+    var minaceDiv3 = document.getElementById("minace3");
 
+    if (plusDiv3.style.display === "none" || plusDiv3.style.display === "") {
+        plusDiv3.style.display = "block";
+        minaceDiv3.style.display = "none";
+    } else {
+        plusDiv3.style.display = "none";
+        minaceDiv3.style.display = "block";
+    }
+}
